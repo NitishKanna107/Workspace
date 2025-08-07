3fb2598fe2b2a3d1793f9805b7dfced16698978a
Nitish Kanna <nitishkanna107@gmail.com>
2025-08-07 06:24:24 +0000

    synchronous bin counter

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # ----------- Configuration Variables -------------
-SRC_DIR="./rtl/bcd_counter.v ./tb/bcd_counter_tb.v"                 # Directory where your Verilog source files are located
-TOP_MODULE="bcd_counter"               # Top-level module name (edit this based on your design)
+SRC_DIR="./rtl/sync_counter.v ./tb/sync_counter_tb.v"                 # Directory where your Verilog source files are located
+TOP_MODULE="sync_counter"               # Top-level module name (edit this based on your design)
 OUT_DIR="./build"              # Directory to store compiled files and waveform
-WAVEFORM="bcdCount_sim.vcd"            # VCD file name for GTKWave
-WAVECONFIG="bcdCount_config.gtkw"
+WAVEFORM="syncCount_sim.vcd"            # VCD file name for GTKWave
+WAVECONFIG="syncCount_config.gtkw"
 
 # Compile the Verilog files
 echo "Compiling Verilog files..."
--- rtl/sync_counter.v
+++ rtl/sync_counter.v
@@ -1,6 +1,21 @@
+module t_ff(
+    input clk, rst, T,
+    output reg Q
+);
+always@(posedge clk, negedge rst)
+begin
+    if (!rst) Q <= 1'b0;
+    else if (T) Q <= ~Q;
+end
+endmodule
+
 module sync_counter(
     input clk, rst,
-    output wire[3:0] 
+    output wire[3:0] A
 );
 
+t_ff T1(clk, rst, 1'b1, A[0]);
+t_ff T2(clk, rst, A[0], A[1]);
+t_ff T3(clk, rst, A[1] & A[0], A[2]);
+t_ff T4(clk, rst, A[1] & A[0] & A[2], A[3]);
 endmodule
